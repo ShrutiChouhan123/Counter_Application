22d5efc053ccdec6216be3b7ac39970433d40ab5
ShrutiChouhan123 <shchouhan@bestpeers.in>
2023-10-11 10:47:27 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Card, Button} from 'react-bootstrap';
+import { Card, Button,Alert} from 'react-bootstrap';
 import React from 'react';
 
 
@@ -8,7 +8,9 @@ class UsingBootsrap extends React.Component {
     super(props)
     this.state = {
       count: 0,
-      show:true
+      // show:false
+      flag:false,
+      dec:false,
     }
 
   }
@@ -18,8 +20,10 @@ class UsingBootsrap extends React.Component {
   }
 
   Decrement = () => {
-    if(this.state.count==0){
-       alert("decrement over")
+    if(this.state.count===0){
+      //  alert("decrement over")
+      this.setState({dec:true})
+      this.setState({flag:false})
     }
     else{
       this.setState({ count: this.state.count - 1 })
@@ -27,14 +31,20 @@ class UsingBootsrap extends React.Component {
    
   }
   Reset = () => {
-    alert("reset data")
+  //  this.setState({show:false})
+    this.setState({flag:true})
+    this.setState({dec:false})
     this.setState({ count: 0 })
+
   }
 
 
   render() {
     return (
       <>
+       {/* {this.state.show ?<Alert>this is alert</Alert>:'' } */}
+       {this.state.flag ? <Alert class="alert alert-success">Reset All the data</Alert>: ''}
+       {this.state.dec ? <Alert class="alert alert-danger">Decrement Over</Alert> : ''}
         <center><h1 style={{ marginTop: "10px" }}>Counter Application</h1></center>
         
         <Card style={{ width: "30%", background: "yellow", marginLeft: "300px", marginTop: "100px", fontSize: "40px" }} >
@@ -42,10 +52,10 @@ class UsingBootsrap extends React.Component {
 
           <Button variant="outline-success" onClick={this.Increment}>Increment</Button>{' '}
           <Button variant="outline-danger" onClick={this.Decrement}>Decrement</Button>{' '}
-          <Button variant="outline-secondary" onClick={this.Reset}>Reset</Button>{' '}
-
+          <Button variant="outline-secondary" onClick={this.Reset}>Reset
+          </Button>{' '}
+          
         </Card>
-
         
       </>
     )
